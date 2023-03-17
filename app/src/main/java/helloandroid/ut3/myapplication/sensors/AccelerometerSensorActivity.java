d3663bd132fe33a2c096ef85e5290c473f8af1a9
Ahmed Baghawitah <ahmedbagh1997@gmail.com>
2023-03-17 09:07:58 +0100

    Add accekerometer

---
+++ app/src/main/java/helloandroid/ut3/myapplication/sensors/AccelerometerSensorActivity.java
@@ -0,0 +1,45 @@
+package helloandroid.ut3.myapplication.sensors;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class AccelerometerSensorActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private float[] values;
+
+    public AccelerometerSensorActivity(SensorManager sensorManager) {
+        this.sensorManager = sensorManager;
+        this.accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.onResume();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        this.values = event.values;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onResume() {
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onPause() {
+        sensorManager.unregisterListener(this);
+    }
+
+    public float[] getLuminosity() {
+        return values;
+    }
+
+    public Sensor getSensor() {
+        return this.accelerometerSensor;
+    }
+}
